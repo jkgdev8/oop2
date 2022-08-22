14230c59cc5a037ae7fbb0105fd9179e99a78b25
jkgdev8 <jkgdev8@gmail.com>
2022-08-21 22:49:25 -0700

    new object litterals

--- index.js
+++ index.js
@@ -1,1 +1,13 @@
 // Object Litterals
+function Circle(radius) {
+    return {
+        radius,
+        draw() {
+            console.log('still draw')
+
+        }
+    }
+}
+
+const cir = new Circle(1)
+console.log(cir);
