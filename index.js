b5ca1bddb41ae6d5307421ac4909124011714cd2
jkgdev8 <jkgdev8@gmail.com>
2022-09-02 12:07:57 -0700

    added

--- index.js
+++ index.js
@@ -1,14 +1,1 @@
 // Object Litterals
-function Circle(radius) {
-    return {
-        radius,
-        draw() {
-            console.log('still draw')
-
-        }
-    }
-}
-
-const circ = new Circle(2);
-console.log(circ.draw());
-
