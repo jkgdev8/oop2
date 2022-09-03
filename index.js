67e12d4aa96680a6b5050649139b4cd8b7769335
jkgdev8 <jkgdev8@gmail.com>
2022-09-03 00:48:48 -0700

    constructor function

--- index.js
+++ index.js
@@ -1,13 +1,47 @@
 // Object Litterals
-const circle = {
-    radius: 1,
-    location: {
-        x: 1,
-        y: 1
-    },
-    draw: function () {
+// const circle = {
+//     radius: 1,
+//     location: {
+//         x: 1,
+//         y: 1
+//     },
+//     draw: function () {
+//         console.log('draw');
+//     }
+// };
+
+// circle.draw();
+
+
+
+
+//Factory Function
+// function createCircle(radius) {
+//     return {
+//         radius,
+//         draw: function () {
+//             console.log('draw');
+//         }
+//     }
+// }
+
+// const circle = createCircle(1);
+// circle.draw()
+
+
+
+
+
+// Constructor Function
+function Circle(radius) {
+    this.radius = radius;
+    this.draw = function () {
         console.log('draw');
     }
-};
+}
 
-circle.draw();
+const cir = new Circle(1);
+console.log(cir);
+console.log(cir.draw()
+);
+
