b843cf87b2cadd64ba0ed73179b3d5b7397c2c2a
jkgdev8 <jkgdev8@gmail.com>
2022-11-20 18:51:39 -0800

    added

--- index.js
+++ index.js
@@ -7,3 +7,13 @@ function Circle(radius) {
 
 const c = new Circle(2);
 console.log(c.draw());
+
+function Circle(radius) {
+    this.radius = radius,
+        this.draw = function () {
+            console.log('draw');
+        }
+}
+
+const c = new Circle(2);
+console.log(c.draw());
